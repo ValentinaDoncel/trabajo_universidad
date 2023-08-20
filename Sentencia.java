c53928373edd9f2db6d0d353ffce084801f5c7f0
ValentinaDoncel <142711492+ValentinaDoncel@users.noreply.github.com>
2023-08-20 09:51:59 -0500

    Add files via upload

---
+++ Sentencia.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.sentencia;
+
+/**
+ *
+ * @author einva
+ */
+public class Sentencia {
+
+    public static void main(String[] args) {
+        for ( int cont=0; cont<=10; cont++) {
+           System.out.println("Estoy dando vueltas con for, estoy en la vuelta: "+ cont);
+        }
+            
+    }
+}
